dbeec301e97435b3df54df057a7104e5ea6bc1b0
Aabhushan47 <khanalaabushan10@gmail.com>
2024-09-22 21:51:41 +0545

    deploy

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT_NO || 3000;
 app.use(
   cors({
     credentials: true,
-    origin: ["https://verification-8rak.vercel.app"],
+    origin: "https://verification-8rak.vercel.app",
   })
 );
 app.use(bodyParser.json());
