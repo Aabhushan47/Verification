c0e8f3f6fa0168c277992bb46776c09b7ab260ac
Aabhushan Khanal <83967531+Aabhushan47@users.noreply.github.com>
2024-09-22 19:49:35 +0545

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,15 @@ app.use(bodyParser.json());
 
 app.use("/api", verifyRoute);
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://code-verification-frontend.vercel.app');  // Specific origin
+  res.setHeader('Access-Control-Allow-Credentials', 'true');  // Allow credentials
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
