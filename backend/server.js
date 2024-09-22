7fc70f15fc9c416158253009274742941bd5ffbf
Aabhushan Khanal <83967531+Aabhushan47@users.noreply.github.com>
2024-09-22 21:07:31 +0545

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT_NO || 3000;
 app.use(
   cors({
     credentials: true,
-    //origin: ["https://verification-rlxl.vercel.app"],
+    origin: ["https://verification-rlxl.vercel.app"],
     //methods: ["POST", "GET"],
   })
 );
