8a19dcac22ac6adca48fa07d6409e26267fab3d2
Aabhushan Khanal <83967531+Aabhushan47@users.noreply.github.com>
2024-09-22 19:40:27 +0545

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT_NO || 3000;
 app.use(
   cors({
     credentials: true,
-    origin: "https://code-verification-frontend.vercel.app/",
+    origin: "https://code-verification-frontend.vercel.app",
   })
 );
 app.use(bodyParser.json());
